33314fe0a777b1345ea51815b496707194b30ce5
ray-python <QHXV09IMgit>
2017-06-11 19:44:42 +0300

    test

--- init.sh
+++ init.sh
@@ -1,3 +1,2 @@
-sudo unlink /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/nginx.conf
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
